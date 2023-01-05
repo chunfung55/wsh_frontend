137b27072e4b4ec521c2fb187e6629ef63774957
Limited icu <icunethk@gmail.com>
2023-01-06 01:15:19 +0800

    init navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,17 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import React from "react";
 import { appWithTranslation } from "next-i18next";
+import Layout from "@/components/layout";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <React.Fragment>
       <Head>
-        <title>next-i18next Sample</title>
+        <title>Wild Senses</title>
       </Head>
-
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </React.Fragment>
   );
 };
