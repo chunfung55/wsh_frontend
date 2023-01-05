77e94bb320c144a8dc3d4f68090611f06be071a4
Limited icu <icunethk@gmail.com>
2023-01-05 20:29:45 +0800

    init multi lang

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+import { i18n } from "./next-i18next.config";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n,
+};
 
-module.exports = nextConfig
+export default nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import React from "react";
+import { AppPropsWithLayout } from "next/app";
+import { appWithTranslation } from "next-i18next";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>next-i18next Sample</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
+};
+
+export default appWithTranslation(App);
