4c36a36824510b911d10638cc0d78f4257b0e4e0
Limited icu <icunethk@gmail.com>
2023-01-30 21:37:06 +0800

    merge dev to test

--- components/ChipButton.tsx
+++ components/ChipButton.tsx
@@ -1,10 +1,9 @@
 import Chip from "@mui/material/Chip";
-import '../styles/Home.module.css';
+import "../styles/Home.module.css";
 import styles from "../styles/Home.module.css";
 
-const ChipButton = ({content}:{content: string;}) =>{
-return(
-<Chip label={content} ></Chip>)
-}
+const ChipButton = ({ content }: { content: string }) => {
+  return <Chip label={content}></Chip>;
+};
 
-export default ChipButton
+export default ChipButton;
--- pages/home/home.tsx
+++ pages/home/home.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import type { AppProps } from "next/app";
 import Layout from "../../components/layout";
 import { appWithTranslation } from "next-i18next";
 
-const home =({ Component, pageProps }: AppProps) => {
-    return (
-        <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    );
-  }
-  
-  export default appWithTranslation(home);
+const home = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
+
+export default appWithTranslation(home);
