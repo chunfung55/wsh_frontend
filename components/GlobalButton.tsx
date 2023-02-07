3bbb564968a8473f13a54ddccdd6c91950c7a6d8
dawnlisk <44919554+dawnlisk@users.noreply.github.com>
2023-02-07 21:31:45 +0800

    Update GlobalButton.tsx

--- components/GlobalButton.tsx
+++ components/GlobalButton.tsx
@@ -1,13 +1,4 @@
 import Button from "@mui/material/Button";
-<<<<<<< HEAD
-import '../styles/Home.module.css';
-import styles from "../styles/Home.module.css";
-
-const GlobalButton = ({name}:{name:string}) =>{
-    return( <Button className={styles.comButton}>{name}</Button>)
-}
-export default GlobalButton
-=======
 import "../styles/Home.module.css";
 import styles from "../styles/Home.module.css";
 
@@ -15,4 +6,4 @@ const GlobalButton = ({ name }: { name: string }) => {
   return <Button className={styles.comButton}>{name}</Button>;
 };
 export default GlobalButton;
->>>>>>> 4c36a36824510b911d10638cc0d78f4257b0e4e0
+
