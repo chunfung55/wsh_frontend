90ac41314fe5e06a2126ba18eb4468dfc0d348cf
Limited icu <icunethk@gmail.com>
2023-04-18 00:10:12 +0800

    fix navbar

---
+++ pages/about-safari-accommodation/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "@/styles/Home.module.css";
+import { useTranslation } from "next-i18next";
+
+import { useRouter } from "next/router";
+import Layout from "@/components/layout";
+import { getDestinationCategories, getMenu } from "@/services/common";
+import { commonGetStaticProps, navBarProps } from "@/interfaces/common";
+
+const AboutSafariAccommodation = ({ navItems }: navBarProps) => {
+  const router = useRouter();
+  const { t } = useTranslation("common");
+  return <Layout navItems={navItems}></Layout>;
+};
+
+export async function getStaticProps({ locale }: commonGetStaticProps) {
+  const [mainMeuns] = await Promise.all([getMenu(locale)]);
+  return {
+    props: {
+      navItems: mainMeuns,
+    },
+  };
+}
+export default AboutSafariAccommodation;
